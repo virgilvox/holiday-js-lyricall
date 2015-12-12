3548383ad9645dfda9a7d52a6a51eb3844efeb17
virgilvox <moheeb.zara@citrix.com>
2015-12-12 13:17:08 -0700

    v1.0.9 ach.js to test alchemy

--- ach.js
+++ ach.js
@@ -7,60 +7,46 @@ require.extensions['.txt'] = function (module, filename) {
 };
 
 var words = require("./test.txt");
+var sentiment, entities, concepts, taxonomies, relations, keywords, category;
 
 alchemy.sentiment(words, {}, function(err, response) {
   if (err) throw err;
-
-  var sentiment = response.docSentiment;
-
+  sentiment = response.docSentiment;
   console.log("SENTIMENT:", sentiment);
 });
 
 alchemy.entities(words, {}, function(err, response) {
   if (err) throw err;
-
-  // See http://www.alchemyapi.com/api/entity/htmlc.html for format of returned object
-  var entities = response.entities;
-
-    console.log("ENTITIES", entities);
+  entities = response.entities;
+  console.log("ENTITIES", entities);
 });
 
 alchemy.relations(words, {}, function(err, response) {
   if (err) throw err;
-
-  // See http://www.alchemyapi.com/api/relation/htmlc.html for format of returned object
-  var relations = response.relations;
+  relations = response.relations;
   console.log("RELATIONS", relations);
 });
 
 alchemy.concepts(words, {}, function(err, response) {
   if (err) throw err;
-
-  // See http://www.alchemyapi.com/api/concept/htmlc.html for format of returned object
-  var concepts = response.concepts;
+  concepts = response.concepts;
   console.log("CONCEPTS", concepts);
 });
 
 alchemy.keywords(words, {}, function(err, response) {
   if (err) throw err;
-
-  // See http://www.alchemyapi.com/api/keyword/htmlc.html for format of returned object
-  var keywords = response.keywords;
+  keywords = response.keywords;
   console.log("KEYWORDS", keywords);
 });
 
 alchemy.taxonomies(words, {}, function(err, response) {
   if (err) throw err;
-
-  // See http://www.alchemyapi.com/api/keyword/htmlc.html for format of returned object
-  var taxonomies = response.taxonomies;
+  taxonomies = response.taxonomies;
   console.log("TAXONOMY", taxonomies);
 });
 
 alchemy.category(words, {}, function(err, response) {
   if (err) throw err;
-
-  // See http://www.alchemyapi.com/api/keyword/htmlc.html for format of returned object
-  var category = response.category;
+  category = response.category;
   console.log("TOPIC CATEGORIZATION", category);
 });
